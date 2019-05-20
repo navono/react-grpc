da8cc79ee79fabbc2bfc4e45115f59302513714d
navono <navono007@gmail.com>
2019-05-20 14:29:11 +0800

    refactor: 修改名称

---
+++ react-grpc/proto/ping_pong_pb.d.ts
@@ -0,0 +1,38 @@
+import * as jspb from "google-protobuf"
+
+export class PingRequest extends jspb.Message {
+  getPing(): string;
+  setPing(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): PingRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: PingRequest): PingRequest.AsObject;
+  static serializeBinaryToWriter(message: PingRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): PingRequest;
+  static deserializeBinaryFromReader(message: PingRequest, reader: jspb.BinaryReader): PingRequest;
+}
+
+export namespace PingRequest {
+  export type AsObject = {
+    ping: string,
+  }
+}
+
+export class PongResponse extends jspb.Message {
+  getPong(): string;
+  setPong(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): PongResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: PongResponse): PongResponse.AsObject;
+  static serializeBinaryToWriter(message: PongResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): PongResponse;
+  static deserializeBinaryFromReader(message: PongResponse, reader: jspb.BinaryReader): PongResponse;
+}
+
+export namespace PongResponse {
+  export type AsObject = {
+    pong: string,
+  }
+}
+
