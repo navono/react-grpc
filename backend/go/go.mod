110b3074681d4b0b632b96fce0f5996c9b333963
navono <navono007@gmail.com>
2019-05-22 15:30:01 +0800

    proto files

--- backend/go/go.mod
+++ backend/go/go.mod
@@ -3,7 +3,10 @@ module go-backend
 go 1.12
 
 require (
+	github.com/golang/protobuf v1.3.1
+	github.com/grpc-ecosystem/grpc-gateway v1.9.0
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
+	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
 )
---
+++ frontend/react-grpc/proto/v1/ping_pong_pb.d.ts
@@ -0,0 +1,81 @@
+import * as jspb from "google-protobuf"
+
+import * as google_api_annotations_pb from '../google/api/annotations_pb';
+import * as protoc$gen$swagger_options_annotations_pb from '../protoc-gen-swagger/options/annotations_pb';
+
+export class PingRequest extends jspb.Message {
+  getPing(): string;
+  setPing(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): PingRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: PingRequest): PingRequest.AsObject;
+  static serializeBinaryToWriter(message: PingRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): PingRequest;
+  static deserializeBinaryFromReader(message: PingRequest, reader: jspb.BinaryReader): PingRequest;
+}
+
+export namespace PingRequest {
+  export type AsObject = {
+    ping: string,
+  }
+}
+
+export class PongResponse extends jspb.Message {
+  getPong(): string;
+  setPong(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): PongResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: PongResponse): PongResponse.AsObject;
+  static serializeBinaryToWriter(message: PongResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): PongResponse;
+  static deserializeBinaryFromReader(message: PongResponse, reader: jspb.BinaryReader): PongResponse;
+}
+
+export namespace PongResponse {
+  export type AsObject = {
+    pong: string,
+  }
+}
+
+export class fetchPingCountRequest extends jspb.Message {
+  getApi(): string;
+  setApi(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): fetchPingCountRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: fetchPingCountRequest): fetchPingCountRequest.AsObject;
+  static serializeBinaryToWriter(message: fetchPingCountRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): fetchPingCountRequest;
+  static deserializeBinaryFromReader(message: fetchPingCountRequest, reader: jspb.BinaryReader): fetchPingCountRequest;
+}
+
+export namespace fetchPingCountRequest {
+  export type AsObject = {
+    api: string,
+  }
+}
+
+export class fetchPingCountResponse extends jspb.Message {
+  getApi(): string;
+  setApi(value: string): void;
+
+  getCount(): number;
+  setCount(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): fetchPingCountResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: fetchPingCountResponse): fetchPingCountResponse.AsObject;
+  static serializeBinaryToWriter(message: fetchPingCountResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): fetchPingCountResponse;
+  static deserializeBinaryFromReader(message: fetchPingCountResponse, reader: jspb.BinaryReader): fetchPingCountResponse;
+}
+
+export namespace fetchPingCountResponse {
+  export type AsObject = {
+    api: string,
+    count: number,
+  }
+}
+
