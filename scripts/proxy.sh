67908e52956860dbd8fc31c0630457218fc8c21e
navono <navono007@gmail.com>
2019-05-22 19:53:58 +0800

    fix: use go backend

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,3 +1,6 @@
-d ../proxy/grpcwebproxy-0.9.5
+#! /bin/bash
+# proxy.sh
+
+cd ../proxy/grpcwebproxy
 go build .
 ./grpcwebproxy.exe --backend_addr=127.0.0.1:8088 --backend_tls_noverify=true --run_tls_server=false --use_websockets=true --allow_all_origins
